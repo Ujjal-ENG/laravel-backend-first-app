57d2dbe2f00e6da68d70ef2833135cfc42bda851
user <ujjalroy7862@gmail.com>
2023-03-22 01:57:17 +0600

    data is successfully created in the data base

--- resources/js/pages/Create.jsx
+++ resources/js/pages/Create.jsx
@@ -1,30 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import http from "../http/http";
 
 const Create = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const { name, email, password } = user;
+
+    const handleChange = (e) => {
+        setUser((ps) => ({
+            ...ps,
+            [e.target.id]: e.target.value,
+        }));
+    };
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        http.post("/users", user).then((res) => {
+            navigate("/");
+        });
+    };
+
     return (
         <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+            <h1 className="text-center">Create New User</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={handleChange}
+                    label="Name"
+                    placeholder="Enter Name"
+                    className="border-2 border-black p-2 rounded-lg m-4 w-1/2"
+                />
+                <br />
+                <label>Email:</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={handleChange}
+                    label="email"
+                    placeholder="Enter Name"
+                    className="border-2 border-black p-2 rounded-lg m-4 w-1/2"
+                />
+                <br />
+                <label>Password:</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={handleChange}
+                    label="password"
+                    placeholder="Enter Name"
+                    className="border-2 border-black p-2 rounded-lg m-4 w-1/2"
+                />
+                <br></br>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Create User
                 </Button>
-            </Form>
+            </form>
         </div>
     );
 };
