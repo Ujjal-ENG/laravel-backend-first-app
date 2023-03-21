fdc6ea6d575be9d788f29260cd929a33a452f6ea
user <ujjalroy7862@gmail.com>
2023-03-22 00:47:56 +0600

    maked the tables

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,12 +1,41 @@
 import React, { useEffect, useState } from "react";
+import http from "../http/http";
+import Table from "react-bootstrap/Table";
 
 const Home = () => {
     const [user, setUser] = useState([]);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        fetchUser();
+    }, []);
 
-    const fetchUser = async () => {};
-    return <div>Home</div>;
+    const fetchUser = async () => {
+        const fetchData = await http.get("/users");
+
+        setUser(fetchData.data);
+    };
+    return (
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Sno.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Ujjal</td>
+                        <td>ujjal@gmail.com</td>
+                        <td>Buttons</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 };
 
 export default Home;
