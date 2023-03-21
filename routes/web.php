a80e5dc6e176c045c83f36f9175cd34ad000ca94
user <ujjalroy7862@gmail.com>
2023-03-21 21:23:08 +0600

    created my first controller on PHP

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/kopa', function () {
-    return view('Kopa');
-});
-Route::get('/product', function () {
-    return view('.products.add.Product');
-});
+})->name("Home");
